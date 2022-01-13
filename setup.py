8dae5d5c53dc22ee8fb864e801be5bf7ac290a18
yuma300 <ywakimoto1s@gmail.com>
2022-01-13 10:41:13 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 from glob import glob
+from os.path import splitext
+from os.path import basename
+
 
 setup(
     name='senkalib',
@@ -11,5 +14,7 @@ setup(
     author_email='ywakimoto1s@gmail.com',
     url='https://github.com/ca3-caaip/senkalib',
 
-    packages=find_packages('src')
+    packages=find_packages('src'),
+    package_dir={'': 'src'},    
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')]    
 )
