0c0bc512c8522c6733b706ec262d7506813d3ba7
yuma300 <ywakimoto1s@gmail.com>
2022-01-17 14:49:26 +0900

    modify osmosis transaction

--- test/chain/osmosis/test_osmosis_transaction.py
+++ test/chain/osmosis/test_osmosis_transaction.py
@@ -4,6 +4,7 @@ from src.senkalib.chain.osmosis.osmosis_transaction import OsmosisTransaction
 import json
 from pathlib import Path
 import os
+from decimal import *
 
 class TestOsmosisTransaction(unittest.TestCase):
   def test_get_timestamp(self):
@@ -12,12 +13,11 @@ class TestOsmosisTransaction(unittest.TestCase):
     timestamp = transaction.get_timestamp()
     self.assertEqual(timestamp, '2022-01-16 09:03:30')
 
-#  def test_get_transaction_fee(self):
-#    header = json.loads(Path('%s/../../testdata/evm/header.json' % os.path.dirname(__file__)).read_text())
-#    receipt = json.loads(Path('%s/../../testdata/evm/transaction_receipt/approve.json' % os.path.dirname(__file__)).read_text())
-#    transaction = OsmosisTransaction(header['hash'], receipt, header['timeStamp'], header['gasUsed'], header['gasPrice'])
-#    fee = transaction.get_transaction_fee()
-#    self.assertEqual(fee, 222150000000000)
+  def test_get_transaction_fee(self):
+    swap_transaction = json.loads(Path('%s/../../testdata/osmosis/swap.json' % os.path.dirname(__file__)).read_text())
+    transaction = OsmosisTransaction(swap_transaction)
+    fee = transaction.get_transaction_fee()
+    self.assertEqual(fee, Decimal('0'))
 
 if __name__ == '__main__':
   unittest.main()
