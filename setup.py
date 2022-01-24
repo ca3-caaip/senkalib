c1157c24c2a33f0bb79fb2edf362e3745e01ff52
yuma300 <ywakimoto1s@gmail.com>
2022-01-24 12:25:30 +0900

    add get_available_chain

--- setup.py
+++ setup.py
@@ -1,8 +1,4 @@
 from setuptools import setup, find_packages
-from glob import glob
-from os.path import splitext
-from os.path import basename
-
 
 def _requires_from_file(filename):
     return open(filename).read().splitlines()
@@ -17,7 +13,9 @@ setup(
     author_email='ywakimoto1s@gmail.com',
     url='https://github.com/ca3-caaip/senkalib',
     install_requires=_requires_from_file('requirements.txt'),
+    extras_require={
+        "test": ["pytest", "pytest-cov"]
+    },
     packages=find_packages('src'),
-    package_dir={'': 'src'},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')]
+    package_dir={'': 'src'}
 )
