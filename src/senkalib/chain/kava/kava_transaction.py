c852f9d3898cbefe9363e4cf19815545528dbca4
regonn <programmer@haizine.art>
2022-04-12 02:29:51 +0000

    adding KavaTransaction class

---
+++ src/senkalib/chain/kava/kava_transaction.py
@@ -0,0 +1,22 @@
+from senkalib.chain.transaction import Transaction 
+from decimal import *
+from datetime import datetime as dt
+
+getcontext().prec = 50
+
+class KavaTransaction(Transaction):
+  chain = 'kava'
+
+  def __init__(self, transaction:dict):
+    super().__init__(transaction["data"]["txhash"])
+    self.transaction = transaction
+
+  def get_timestamp(self) -> str:
+    return str(dt.strptime(self.transaction['header']['timestamp'], '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=None))
+
+  def get_transaction_fee(self) -> Decimal:
+    fee_list = self.transaction['data']['tx']['auth_info']['fee']['amount']
+    return Decimal('0') if len(fee_list) == 0 else Decimal(fee_list[0]['amount'])
+
+  def get_transaction(self) -> dict:
+    return self.transaction
