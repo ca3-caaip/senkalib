55ee20aa30da6728e6741b0ef55f56668bc698d9
Kou Matsumoto <develop@kou-matsumoto.jp>
2022-04-21 19:57:56 +0900

    feat: sort loaded transactions by timerange and block id range

--- src/senkalib/chain/osmosis/osmosis_transaction_generator.py
+++ src/senkalib/chain/osmosis/osmosis_transaction_generator.py
@@ -1,36 +1,63 @@
-from senkalib.chain.transaction import Transaction
+from dataclasses import dataclass
+from dateutil import parser
 from senkalib.chain.transaction_generator import TransactionGenerator
 from senkalib.senka_setting import SenkaSetting
 from senkalib.chain.osmosis.osmosis_transaction import OsmosisTransaction
-from typing import List
+from math import inf
 import requests
 
 class OsmosisTransactionGenerator(TransactionGenerator):
   chain = 'osmosis'
 
   @classmethod
-  def get_transactions(cls, settings:SenkaSetting, address:str, timerange:dict = None, blockrange:dict = None) -> List[Transaction]:
-    osmosis_transactions = []
-    num_transactions = 50
+  def get_transactions(cls, settings:SenkaSetting, address:str, timerange:dict = None, blockrange:dict = None) -> list[OsmosisTransaction]:
     startblock = int(blockrange['startblock']) if blockrange is not None and 'startblock' in blockrange and type(blockrange['startblock']) is int else 0
-    endblock = int(blockrange['endblock']) if blockrange is not None and 'endblock' in blockrange and type(blockrange['endblock']) is int else None
+    endblock = int(blockrange['endblock']) if blockrange is not None and 'endblock' in blockrange and type(blockrange['endblock']) is int else inf
+    starttime = int(timerange['starttime']) if timerange is not None and 'starttime' in timerange and type(timerange['starttime']) is int else 0
+    endtime = int(timerange['endtime']) if timerange is not None and 'endtime' in timerange and type(timerange['endtime']) is int else inf
 
-    while num_transactions >= 50:
-      transactions = OsmosisTransactionGenerator.get_txs(address, startblock)
-      num_transactions = len(transactions)
-      for transaction in transactions:
-        startblock = transaction['header']['id']
-        if endblock is not None and startblock <= endblock:
-          return osmosis_transactions
-        osmosis_transactions.append(OsmosisTransaction(transaction))
+    total_result = []
+    id_cursor = get_nearest_id(endtime)
 
+    while True:
+      result = cls.get_txs(address, id_cursor)
+      for tx in result:
+        id_cursor = int(tx['header']['id'])
+        block_id = int(tx['data']['height'])
+        time = to_timestamp(tx['header']['timestamp'])
 
-    return osmosis_transactions
+        if starttime <= time <= endtime and startblock <= block_id <= endblock:
+          total_result.append(OsmosisTransaction(tx))
+
+        if time < starttime or block_id < startblock:
+          return total_result
+        else:
+          continue
+
+      if len(result) < 50:
+        return total_result
 
   @classmethod
-  def get_txs(cls, address, startblock):
-    response = requests.get(
-        'https://api-osmosis.cosmostation.io/v1/account/new_txs/%s' % address,
-        params={'from': startblock, 'limit': 50})
-    transactions = response.json()
-    return transactions
+  def get_txs(cls, address: str, id_from: int) -> list[dict]:
+    return requests.get('https://api-osmosis.cosmostation.io/v1/account/new_txs/%s' % address, params={'from': id_from, 'limit': 50}).json()
+
+
+@dataclass
+class IdRecord:
+  timestamp: int
+  id: int
+
+id_records: list[IdRecord] = [
+  IdRecord(1641027605, 11843943),  # 2022-01-01T09:00:05Z
+  IdRecord(1640995203, 11802509),  # 2022-01-01T00:00:03Z
+  IdRecord(1624052574, 2),         # 2021-06-18T21:42:54Z
+]
+
+def get_nearest_id(time: int):
+  for newer, older in zip(id_records, id_records[1:]):  # NOTE: replace zip with pairwise at python@3.10
+    if older.timestamp <= time < newer.timestamp:
+      return newer.id
+  return 0
+
+def to_timestamp(iso8601string: str) -> int:
+  return int(parser.parse(iso8601string).timestamp())
--- test/chain/osmosis/test_osmosis_transaction_generator.py
+++ test/chain/osmosis/test_osmosis_transaction_generator.py
@@ -1,32 +1,51 @@
 import unittest
-from unittest.mock import *
-from src.senkalib.chain.osmosis.osmosis_transaction_generator import OsmosisTransactionGenerator
+from src.senkalib.chain.osmosis.osmosis_transaction_generator import OsmosisTransactionGenerator, get_nearest_id, to_timestamp, id_records
 import json
 from pathlib import Path
 import os
 from src.senkalib.senka_setting import SenkaSetting
 from unittest.mock import *
 from decimal import *
+from itertools import repeat
 
 class TestOsmosisTransactionGenerator(unittest.TestCase):
-  def test_get_transactions(self):
+  @patch.object(OsmosisTransactionGenerator, 'get_txs')
+  def test_get_transactions(self, get_txs):
+    dummy_txs = json.loads(Path('%s/../../testdata/chain/osmosis/test_transactions.json' % os.path.dirname(__file__)).read_text())
     settings = SenkaSetting({})
+    # check results should be expected data
+    get_txs.return_value = dummy_txs
+    txs = OsmosisTransactionGenerator.get_transactions(settings, 'address', None, {})
+    tx = txs[0]
+    assert len(txs) == 44
+    assert tx.get_timestamp() == '2022-01-15 12:18:55'
+    assert tx.get_transaction_fee() == Decimal(33)
+    # filter results by parameters
+    assert len(OsmosisTransactionGenerator.get_transactions(settings, 'address', None, {'startblock': 2781756})) == 43
+    assert len(OsmosisTransactionGenerator.get_transactions(settings, 'address', None, {'endblock': 2781756})) == 4
+    assert len(OsmosisTransactionGenerator.get_transactions(settings, 'address', {'starttime': to_timestamp('2022-01-15T12:18:54Z')})) == 2
+    assert len(OsmosisTransactionGenerator.get_transactions(settings, 'address', {'endtime': to_timestamp('2022-01-15T12:18:54Z')})) == 42
+    # get_txs should be called multiply if result length gte 50
+    get_txs.reset_mock()
+    tx = dummy_txs[0]
+    get_txs.side_effect = [list(repeat(tx, 50)), list(repeat(tx, 49))]
+    txs = OsmosisTransactionGenerator.get_transactions(settings, 'address', None, {})
+    assert len(txs) == 99
+    assert get_txs.call_count == 2
 
-    with patch.object(OsmosisTransactionGenerator, 'get_txs', new=TestOsmosisTransactionGenerator.mock_get_txs):
-      transactions = OsmosisTransactionGenerator.get_transactions(settings, 'osmo1xq5du8upw2fmyx7h43w8uqv47vln70hre92wvm', None, {})
-      timestamp = transactions[0].get_timestamp()
-      fee = transactions[0].get_transaction_fee()
-      self.assertEqual(len(transactions), 44)
-      self.assertEqual(timestamp, '2022-01-15 12:18:55')
-      self.assertEqual(fee, Decimal(33))
+class TestOsmosisTransactionGeneratorInternals:
+  def test_get_nearest_id(self):
+    newest = id_records[0]
+    second = id_records[1]
+    assert get_nearest_id(newest.timestamp + 1) == 0
+    assert get_nearest_id(newest.timestamp + 0) == 0
+    assert get_nearest_id(newest.timestamp - 1) == newest.id
+    assert get_nearest_id(second.timestamp + 1) == newest.id
+    assert get_nearest_id(second.timestamp + 0) == newest.id
+    assert get_nearest_id(second.timestamp - 1) == second.id
 
-      transactions = OsmosisTransactionGenerator.get_transactions(settings, 'osmo1xq5du8upw2fmyx7h43w8uqv47vln70hre92wvm', None, {'endblock': 14700510})
-      self.assertEqual(len(transactions), 40)
-
-  @classmethod
-  def mock_get_txs(cls, settings:dict, address:str, arg_startblock:int = None, arg_endblock:int = None, arg_page:int = None):
-    transactions = json.loads(Path('%s/../../testdata/chain/osmosis/test_transactions.json' % os.path.dirname(__file__)).read_text())
-    return transactions
+  def test_to_timestamp(self):
+    assert to_timestamp('2022-01-01T08:59:59Z') == 1641027599
 
 if __name__ == '__main__':
   unittest.main()
