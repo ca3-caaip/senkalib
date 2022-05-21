f7f1719a22b8de4ca5204c7904f797fdc7f1afb6
watagori <cottongorillla@gmail.com>
2022-05-21 11:24:26 +0900

    feat: validate bitbank exchange data

--- src/senkalib/chain/bitbank/bitbank_transaction_generator.py
+++ src/senkalib/chain/bitbank/bitbank_transaction_generator.py
@@ -1,8 +1,14 @@
 import csv
+from enum import Enum, auto
 
 from senkalib.chain.bitbank.bitbank_transaction import BitbankTransaction
 from senkalib.chain.transaction_generator import TransactionGenerator
 from senkalib.senka_setting import SenkaSetting
+
+
+class DataType(Enum):
+    exchange = auto()
+    unknown = auto()
 
 
 class BitbankTransactionGenerator(TransactionGenerator):
@@ -12,16 +18,26 @@ class BitbankTransactionGenerator(TransactionGenerator):
     def get_transaction_from_csv(
         cls, settings: SenkaSetting, data: str
     ) -> list[BitbankTransaction]:
-        reader = csv.DictReader(data.splitlines())
-        dict_data = [row for row in reader]
-        data_with_type = list(map(cls.set_data_type, dict_data))
-        return list(map(BitbankTransaction, data_with_type))
+        if BitbankTransactionGenerator._validate(data) == DataType.exchange:
+            reader = csv.DictReader(data.splitlines())
+            dict_data = [row for row in reader]
+            data_with_type = list(map(cls._set_data_type, dict_data))
+            return list(map(BitbankTransaction, data_with_type))
+
+        else:
+            raise ValueError("Invalid data")
 
     @staticmethod
-    def set_data_type(data: dict) -> dict:
+    def _set_data_type(data: dict) -> dict:
         data["data_type"] = "bitbank_exchange"
         return data
 
     @staticmethod
-    def validate_exchange(data: dict) -> bool:
-        return True
+    def _validate(data: str) -> DataType:
+        header = csv.reader(data.splitlines()).__next__()
+        if set(header) == set(
+            ["注文ID", "取引ID", "通貨ペア", "タイプ", "売/買", "数量", "価格", "手数料", "M/T", "取引日時"]
+        ):
+            return DataType.exchange
+        else:
+            return DataType.unknown
