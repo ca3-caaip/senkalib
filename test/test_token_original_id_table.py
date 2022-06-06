5c57e2a870a9af9edc5b317950767e2174216d48
watagori <cottongorillla@gmail.com>
2022-06-06 12:15:59 +0900

    feat: set "" in False positoin

--- test/test_token_original_id_table.py
+++ test/test_token_original_id_table.py
@@ -102,7 +102,7 @@ class TestTokenOriginalIdTable(unittest.TestCase):
                 "description": "native token for cosmos",
                 "platform": "bsc",
                 "original_id": "0x0Eb3a705fc54725037CC9e008bDede697f62F335",
-                "primary": None,
+                "primary": "",
             },
             {
                 "symbol_uuid": "e7816a15-ce91-0aa8-0508-21d0d19f3aa8",
@@ -118,7 +118,7 @@ class TestTokenOriginalIdTable(unittest.TestCase):
                 "description": "native token for juno",
                 "platform": "osmosis",
                 "original_id": "ibc/46B44899322F3CD854D2D46DEEF881958467CDD4B3B10086DA49296BBED94BED",
-                "primary": None,
+                "primary": "",
             },
             {
                 "symbol_uuid": "d7fdee9c-13cf-afd3-e793-33d56c10fc12",
@@ -126,7 +126,7 @@ class TestTokenOriginalIdTable(unittest.TestCase):
                 "description": "native token for terra",
                 "platform": "osmosis",
                 "original_id": "ibc/0EF15DF2F02480ADE0BB6E85D9EBB5DAEA2836D3860E9F97F9AADE4F57A31AA0",
-                "primary": None,
+                "primary": "",
             },
             {
                 "symbol_uuid": "432374c6-5e69-eb86-10d9-8c49176aeb1b",
@@ -134,7 +134,7 @@ class TestTokenOriginalIdTable(unittest.TestCase):
                 "description": "doller peg token on terra",
                 "platform": "osmosis",
                 "original_id": "ibc/BE1BB42D4BE3C30D50B68D7C41DB4DFCE9678E8EF8C539F6E6A9345048894FCC",
-                "primary": None,
+                "primary": "",
             },
             {
                 "symbol_uuid": "c0c8e177-53c3-c408-d8bd-067a2ef41ea7",
@@ -150,7 +150,7 @@ class TestTokenOriginalIdTable(unittest.TestCase):
                 "description": "Umee is a cross platform DeFi hub that interconnects between blockplatforms",
                 "platform": "osmosis",
                 "original_id": "ibc/67795E528DF67C5606FC20F824EA39A6EF55BA133F4DC79C90A8C47A0901E17C",
-                "primary": None,
+                "primary": "",
             },
             {
                 "symbol_uuid": "0449e895-da15-a096-7300-00c666088b39",
@@ -158,7 +158,7 @@ class TestTokenOriginalIdTable(unittest.TestCase):
                 "description": "Native token for secret network",
                 "platform": "osmosis",
                 "original_id": "ibc/0954E1C28EB7AF5B72D24F3BC2B47BBB2FDF91BDDFD57B74B99E133AED40972A",
-                "primary": None,
+                "primary": "",
             },
             {
                 "symbol_uuid": "b5a385b2-4599-24be-adb9-0797c565e906",
@@ -166,7 +166,7 @@ class TestTokenOriginalIdTable(unittest.TestCase):
                 "description": "native token for crypto.org",
                 "platform": "osmosis",
                 "original_id": "ibc/E6931F78057F7CC5DA0FD6CEF82FF39373A6E0452BF1FD76910B93292CF356C1",
-                "primary": None,
+                "primary": "",
             },
             {
                 "symbol_uuid": "58311b55-9ccc-a50c-8ad7-d77e5798b345",
@@ -174,7 +174,7 @@ class TestTokenOriginalIdTable(unittest.TestCase):
                 "description": "native token for Persistence",
                 "platform": "osmosis",
                 "original_id": "ibc/A0CC0CF735BFB30E730C70019D4218A1244FF383503FF7579C9201AB93CA9293",
-                "primary": None,
+                "primary": "",
             },
             {
                 "symbol_uuid": "377b9a23-1cb1-e4b5-4f03-910e3e438d34",
@@ -182,7 +182,7 @@ class TestTokenOriginalIdTable(unittest.TestCase):
                 "description": "native token for akash",
                 "platform": "osmosis",
                 "original_id": "ibc/1480B8FD20AD5FCAE81EA87584D269547DD4D436843C1D20F15E00EB64743EF4",
-                "primary": None,
+                "primary": "",
             },
             {
                 "symbol_uuid": "f12c88a1-b54d-d867-9d8f-78fa8960de4a",
@@ -190,7 +190,7 @@ class TestTokenOriginalIdTable(unittest.TestCase):
                 "description": "native token for regen network",
                 "platform": "osmosis",
                 "original_id": "ibc/1DCC8A6CB5689018431323953344A9F6CC4D0BFB261E88C9F7777372C10CD076",
-                "primary": None,
+                "primary": "",
             },
             {
                 "symbol_uuid": "85f9ab60-60c7-9a98-bbb3-bba925452953",
@@ -198,7 +198,7 @@ class TestTokenOriginalIdTable(unittest.TestCase):
                 "description": "native token for sentinel",
                 "platform": "osmosis",
                 "original_id": "ibc/9712DBB13B9631EDFA9BF61B55F1B2D290B2ADB67E3A4EB3A875F3B6081B3B84",
-                "primary": None,
+                "primary": "",
             },
         ]
         return token_original_id_table
